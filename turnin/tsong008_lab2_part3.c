985400470e40432a50c45c82326940e1718a54f5
Tinghui Song <tinghuigz10@gmail.com>
2020-10-20 18:40:33 -0700

    Completed part 4

--- turnin/tsong008_lab2_part3.c
+++ turnin/tsong008_lab2_part3.c
@@ -1,7 +1,7 @@
 /*	Author: Tinghui Song
  *  Partner(s) Name: none
  *	Lab Section:24
- *	Assignment: Lab #2  Exercise #2
+ *	Assignment: Lab #2  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
